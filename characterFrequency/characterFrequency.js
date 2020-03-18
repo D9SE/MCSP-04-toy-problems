257521e2567a99e556245fef485560480b11ee15
D9SE <daltonsoohoo9@gmail.com>
2020-03-18 11:19:33 -0500

    tried today's

--- characterFrequency/characterFrequency.js
+++ characterFrequency/characterFrequency.js
@@ -7,4 +7,17 @@
 // "mississippi" => [ [ "i", 4 ], [ "s", 4 ], [ "p", 2 ], [ "m", 1 ] ]
 // "" => [ ]
 
-function characterFrequency() {}
+function characterFrequency(str) {
+    var counter = 0 //this will be on the end
+    var bigArr = [] //this holds all the smaller arrays
+    for(var i = 0; i < str.length; i++){ //iterate through the string
+        var innerArr = [] //create a new arr to hold the letter
+        innerArr.push(str.push(str[i])) //push that letter into the inner array
+        innerArr.push(counter)
+        bigArr.push(innerArr) //push that into the bigger array
+    }
+
+
+
+    return bigArr
+}
