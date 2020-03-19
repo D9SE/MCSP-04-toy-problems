9f46bc876131784b238fbbdc3f7ff79fd9ddaced
D9SE <daltonsoohoo9@gmail.com>
2020-03-19 09:55:25 -0500

    doneish

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -13,5 +13,18 @@
  */
 
 var allAnagrams = function(string) {
-  // Your code here.
+  //make an array to return
+  //need to find out the length of the string - .length
+  // for each letter, modify the return array with that letter starting
+  // then push the rest of the letters
+  //might need 2 for loops to iterate and place the secondary letters
+  var returnArr = [] //what I will return
+  var length = string.length //3 for example
+    for(var i = 0; i < length; i++){
+      returnArr.push(string[i])
+    }
+    for(var j = 0; j < length; j++){
+      returnArr.push(string[j])
+    }
+  return returnArr.join('')
 };
