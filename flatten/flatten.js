9d3e4dbc57304dbe133d5d53263841a0e722ee93
D9SE <daltonsoohoo9@gmail.com>
2020-03-24 09:45:54 -0500

    why are these so hard

--- flatten/flatten.js
+++ flatten/flatten.js
@@ -3,4 +3,26 @@
 // Example:
 // flatten([1,['a', 2, [8, true], 7]]) returns [1, 'a', 2, 8, true, 7]
 
-function flatten(array) {}
+function flatten(array) {
+    //if array has an array as an element, get the elements out of the inner array and put it back into the outter one. 
+    var returnArr = []
+    for(var i = 0; i < array.length; i++){
+        if(array[i].length === undefined){
+            returnArr.push(array[i])
+        }
+        if(array[i].length !== undefined){ //if there is an array at any of the spots
+            var innerArr = array[i] //put the array in a vari
+            
+        }
+        
+    }
+
+
+
+
+
+
+
+
+    return returnArr
+}
