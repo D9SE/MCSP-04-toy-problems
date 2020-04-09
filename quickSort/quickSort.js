8eb6139b6bfcfb7c34c261c05a15c18ebf5fb904
D9SE <daltonsoohoo9@gmail.com>
2020-04-09 09:48:35 -0500

    quicksort

--- quickSort/quickSort.js
+++ quickSort/quickSort.js
@@ -10,4 +10,17 @@
  *    Combines the two arrays and the pivot into a sorted array.
  */
 
-var quicksort = function(array) {};
+var quicksort = function(array) {
+    var pivot = array[0]
+    var smaller = []
+    var bigger = []
+    for(var i = 1; i < array.length; i++){
+        if(array[i] >= pivot){
+            bigger.push(array[i])
+        }
+        if(array[i] < pivot){
+            smaller.push(array[i])
+        }
+    }
+  return quicksort(smaller).concat(pivot).concat(quicksort(bigger))
+};
