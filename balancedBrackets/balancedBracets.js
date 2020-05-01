d95fcbcf35a09ce07bf773175cf361afe7ac0a9e
D9SE <daltonsoohoo9@gmail.com>
2020-05-01 09:49:04 -0500

    balanced brackets

--- balancedBrackets/balancedBracets.js
+++ balancedBrackets/balancedBracets.js
@@ -9,4 +9,71 @@
 // str: "[{()}]"                  |   true
 // str: "(50)("                   |   false
 // str: "[{]}"                    |   false
-const isBalanced = str => {};
+const isBalanced = str => {
+
+};
+
+
+
+/*
+I string with parenthesis, curlys or square brackets in it.
+O A true/false based on if the number of brackets is even.
+C
+E if no brackets, return true
+
+create a function
+    check if the number of parenthesis, curly or square brackets is even or not. 
+      create counters
+        if even
+            return true
+        if not 
+            return false
+        if none 
+            return true
+
+
+ function isBalanced(str){
+     parCounter = 0;
+     curlCounter = 0;
+     squaCounter = 0;
+    
+     //established my counters, this will be how I test if the number of brackets is balanced or not.
+
+    for(var i = 0; i < str.length; i++){
+        if(str[i] === '(' || str[i] === ')'){
+            parCounter += 1;
+        };
+        if(str[i] === '{' || str[i] === '}'){
+            curlCounter += 1;
+        };
+        if(str[i] === '[' || str[i] === ']'){
+            squaCounter += 1;
+        };
+    }
+
+    // using a for loop, I will iterate over the string to check each position, if they match either an opening or a closing bracket, add one to the counter. 
+
+    if(parCounter > 0 || curlCounter > 0 || squaCounter > 0){
+        if(parCounter % 2 === 0 && curlCounter % 2 === 0 && squaCounter % 2 === 0){
+            return true
+        }
+        else if(parCounter % 2 === 0){
+            return true
+        }
+        else if(curlCounter % 2 === 0){
+            return true
+        }
+        else if(squaCounter % 2 === 0){
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
+    //in this conditional, first I'm checking if 
+
+
+    return true
+ }
+*/
--- maxConsecutiveOnes/maxConsecutiveOnes.js
+++ maxConsecutiveOnes/maxConsecutiveOnes.js
@@ -2,6 +2,68 @@
 
 //maxConsecutiveOnes([1,1,2,0,3,1,1,1,1,5,6,3]) returns 4
 
-function maxConsecutiveOnes(array) {
-  //write solution code
+const consecutiveOnes = array => {
+  //create counter
+    let counter = 0;
+  //for loop to iterate over the array
+    for(let i = 0; i < array.length; i++){
+      //if the element + 1 = 1, incriment the counter 
+      if(counter === 0 && array[i] === 1){
+        counter += 1
+        if(array[i+1] !== 1){
+          //if the element + 1 !== 1, do nothing.
+          counter = 1
+        }
+      }
+      //if a later element = 1, restart the incrimentation
+        if(counter === 1 && array[i] === 1 && (array[i+1] === 1 || array[i-1])){
+        counter +=1
+      }
+    }
+    //when for loop closes, return counter
+  return counter
+  }
+
+
+
+
+/*
+I an array of numbers, possibly in that array are consecutive number 1's
+O returns the number of consecutive 1's
+C
+E if no ones, return 0
+
+create function
+  make a counter to incriment when a one is discovered.
+    for loop to iterate over the array
+      if the element + 1 = 1, incriment the counter
+      if the element + 1 !== 1, do nothing.
+      if a later element = 1, restart the incrimentation
+    when for loop closes
+  return counter
+
+create a function
+
+const consecutiveOnes = array => {
+//create counter
+  let counter = 0;
+//for loop to iterate over the array
+  for(let i = 0; i < array.length; i++){
+    //if the element + 1 = 1, incriment the counter 
+    if(counter === 0 && array[i] === 1){
+      counter += 1
+      if(array[i+1] !== 1){
+        //if the element + 1 !== 1, do nothing.
+        counter = 1
+      }
+    }
+    //if a later element = 1, restart the incrimentation
+    if(counter === 1 && array[i] === 1 && array[i+1] === 1){
+      counter +=1
+    }
+  }
+  //when for loop closes, return counter
+return counter
 }
+
+*/
